662db3120c3a57b1ba2e3949253ea76157aaf618
Hridoymozumder24 <52328719+Hridoymozumder24@users.noreply.github.com>
2021-09-17 21:58:43 +0600

    sdfs

---
+++ app.js
@@ -0,0 +1,11 @@
+const loadQuote = () => {
+          fetch('https://api.kanye.rest')
+          .then(res => res.json())
+          .then(data => displayData(data))
+}
+
+const displayData = quote =>{
+          
+          const quoteElement = document.getElementById('quote')
+          quoteElement.innerText = quote.quote;
+} 
